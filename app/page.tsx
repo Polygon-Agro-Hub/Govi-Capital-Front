00ee816040ed848199a24f654f68c4085ada0bbe
Pasan-FDo <pasanfernando.work@gmail.com>
2025-10-02 13:15:45 +0530

    Add SVG assets and TypeScript configuration
    - Added flowbite.svg, pattern-dark.svg, pattern-light.svg, and vercel.svg to the public directory.
    - Introduced TypeScript configuration file (tsconfig.json) with strict settings and module resolution for Next.js.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { DarkThemeToggle } from "flowbite-react";
+import Image from "next/image";
+
+export default function Home() {
+  
+
+  return (
+    <main className="flex flex-col h-full w-full justify-center items-center">
+      <p className="text-2xl text-red-700">Welcome to Govi Capital</p>
+    </main>
+  );
+}
