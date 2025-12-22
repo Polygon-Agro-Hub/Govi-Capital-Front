bf779ade48d74c3831c948d0424c62fd6a0ab790
Polygon-Code-Hub <polygonholdingsdev@gmail.com>
2025-12-22 13:36:17 +0530

    Refactor next.config.ts with environment-based settings
    Updated next.config.ts to include logging and modified basePath and assetPrefix based on environment variables. Removed export withFlowbiteReact.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,54 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
+console.log(
+  `process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH || '/market'`,
+  process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH || '/market'
+);
+
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/404",
+        destination: "/error/404",
+        permanent: false,
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  basePath: process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH ?? '/govicapital',
+  assetPrefix: process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH ?? '/govicapital',
+  trailingSlash: true,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pub-79ee03a4a23e4dbbb70c7d799d3cb786.r2.dev",
+        hostname: "**",
       },
     ],
   },
 };
 
-export default withFlowbiteReact(nextConfig);
+
+export default nextConfig;
+
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "pub-79ee03a4a23e4dbbb70c7d799d3cb786.r2.dev",
+//       },
+//     ],
+//   },
+// };
+
+// export default withFlowbiteReact(nextConfig);
